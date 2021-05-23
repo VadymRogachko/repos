aa8a4b22d0996da0622ed2a699350569e21c687a
VadymRogachko <vadymrogachko@gmail.com>
2021-05-23 13:55:33 +0300

    Structures

---
+++ C#/Home Work/10. Structures/01/Notebook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace _01
+{
+	struct Notebook
+	{
+		private string model;
+		private string manufacturer;
+		private decimal price;
+
+		public Notebook(string model, string manufacturer, decimal price)
+		{
+			this.model = model;
+			this.manufacturer = manufacturer;
+			this.price = price;
+		}
+
+		public string Model
+		{
+			get
+			{
+				return model;
+			}
+		}
+
+		public string Manufacturer
+		{
+			get
+			{
+				return manufacturer;
+			}
+		}
+
+		public decimal Price
+		{
+			get
+			{
+				return price;
+			}
+		}
+
+		public void Print()
+		{
+			Console.WriteLine($"Модель: {model}\nПроизводитель: {manufacturer}\nЦена: {price}");
+		}
+	}
+}
---
+++ C#/Home Work/10. Structures/01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+Используя Visual Studio, создайте проект по шаблону Console Application.  
+Создайте структуру с именем - Notebook.  
+Поля структуры: модель, производитель, цена.  
+В структуре должен быть реализован конструктор для инициализации полей и метод для вывода содержимого полей на экран.  
+ */
+
+namespace _01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Notebook notebook = new Notebook("NV 6530", "LG", 22000);
+			notebook.Print();
+
+			Console.ReadKey();
+		}
+	}
+}
